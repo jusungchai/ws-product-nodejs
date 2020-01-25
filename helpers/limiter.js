b1fdcdae20c60e704cef029280f000def0a18bfe
Jay <chunsaa@gmail.com>
2020-01-25 00:36:27 -0500

    fix timer issue

--- helpers/limiter.js
+++ helpers/limiter.js
@@ -1,6 +1,5 @@
 const rateLimit = require('./rateLimit')
 
-
 //Limit timer is based on the request IP address
 //Due to unique IP check, saving to object may not be optimal for scaling and should be added to a database
 const limiter = (req, res, next) => {
@@ -13,20 +12,17 @@ const limiter = (req, res, next) => {
     };
   }
 
+  if (rateLimit.users[req.ip].remain === 3){
+    setTimeout(() => { 
+      rateLimit.users[req.ip].remain = requestLimit;      
+    }, timeOut * 1000);
+  }  
+
   rateLimit.users[req.ip].remain--;
 
   res.setHeader("X-RateLimit-Remaining", rateLimit.users[req.ip].remain);
 
-  if (rateLimit.users[req.ip].remain < 0){
-    if (rateLimit.users[req.ip].remain === -1){
-      setTimeout(() => { 
-        rateLimit.users[req.ip].remain = requestLimit;      
-      }, timeOut * 1000);
-    }    
-    return res.sendStatus(429);
-  }
-
-  next();
+  rateLimit.users[req.ip].remain < 0 ? res.sendStatus(429) : next();
 }
 
 module.exports = limiter;
